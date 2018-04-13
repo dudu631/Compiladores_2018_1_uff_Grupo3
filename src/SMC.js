83715a63ddffccfb23ef9d5569b2bd1bc1c4a943
dudu631 <eduardo9ramos@hotmail.com>
2018-04-12 22:52:52 -0300

    Adicionando estrutura de SMC e implementando servidor

---
+++ src/SMC.js
@@ -0,0 +1,37 @@
+﻿
+class SMC {
+    constructor(S, M, C) {
+        this.S = S;
+        this.M = M;
+        this.C = C;
+    }
+    
+    empilhaControle(x) {
+        this.C.push(x);
+    }
+
+    desempilhaControle() {
+        return this.C.pop();
+    }
+
+    empilhaValor(x) {
+        this.S.push(x);
+    }
+
+    desempilhaValor() {
+        return this.S.pop();
+    }
+
+    acessaMemoria(key) {
+        return this.M.get(key);
+    }
+
+    guardaMemoria(key,x) {
+        this.M.set(key, x);
+    }
+
+};
+
+module.exports = SMC;
+
+
--- src/app.js
+++ src/app.js
@@ -1,10 +1,44 @@
 var peg = require("pegjs");
 var fs = require("fs");
+const SMC = require("./SMC.js");
+
+var http = require('http');
 
 var parser = peg.generate(fs.readFileSync("./grammar.pegjs", 'utf8'));
 
+var tree = parser.parse("a * b * c");
+
+eval(tree, new SMC([], new Map(), []));
+
+setupServer();
+
+//x.empilhaControle(2);
+//x.guardaMemoria("variavel1", 22);
 
 console.log("Imprimindo: ");
-console.log(parser.parse("{a:=b}"));
-console.log(parser.parse("{if ae==ea print(a) else a:=b}"));
+//console.log(x.desempilhaControle());
 
+console.log("Imprimindo 2:")
+//console.log(x.acessaMemoria("variavel1"));
+
+
+
+function eval(tree, smc) {
+
+
+}
+
+function setupServer() {
+    const PORT = 8080;
+
+    fs.readFile('./index.html', function (err, html) {
+
+        if (err) throw err;
+
+        http.createServer(function (request, response) {
+            response.writeHeader(200, { "Content-Type": "text/html" });
+            response.write(html);
+            response.end();
+        }).listen(PORT);
+    });
+}
