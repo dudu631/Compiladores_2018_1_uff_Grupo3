51ac4b0344e614320128e8487f1ed624d7f9aaff
dudu631 <eduardo9ramos@hotmail.com>
2018-04-13 01:37:30 -0300

    Remocao de testes em relacao ao NodeJS

--- src/app.js
+++ src/app.js
@@ -1,26 +1,24 @@
 var peg = require("pegjs");
 var fs = require("fs");
 const SMC = require("./SMC.js");
-
 var http = require('http');
 
 var parser = peg.generate(fs.readFileSync("./grammar.pegjs", 'utf8'));
 
-var tree = parser.parse("a * b * c");
+var tree = parser.parse("a * b * (c-4)");
 
 eval(tree, new SMC([], new Map(), []));
-
-setupServer();
 
 //x.empilhaControle(2);
 //x.guardaMemoria("variavel1", 22);
 
-console.log("Imprimindo: ");
+console.log(3 / (5 - 9) / 2);
 //console.log(x.desempilhaControle());
 
 console.log("Imprimindo 2:")
 //console.log(x.acessaMemoria("variavel1"));
 
+//console.log(2 - 8 / 4 + 5);
 
 
 function eval(tree, smc) {
@@ -28,17 +26,4 @@ function eval(tree, smc) {
 
 }
 
-function setupServer() {
-    const PORT = 8080;
 
-    fs.readFile('./index.html', function (err, html) {
-
-        if (err) throw err;
-
-        http.createServer(function (request, response) {
-            response.writeHeader(200, { "Content-Type": "text/html" });
-            response.write(html);
-            response.end();
-        }).listen(PORT);
-    });
-}
