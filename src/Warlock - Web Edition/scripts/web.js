cda0bc3a3ede4f121a0c7984798e8542f2a99548
dudu631 <eduardo9ramos@hotmail.com>
2018-04-27 10:19:00 -0300

    Novo layout de abas

--- src/Warlock - Web Edition/scripts/web.js
+++ src/Warlock - Web Edition/scripts/web.js
@@ -65,10 +65,15 @@ function openTab(evt, name) {
     // Get all elements with class="tablinks" and remove the class "active"
     tablinks = document.getElementsByClassName("tablinks");
     for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
+		var idAba = "#"+ tablinks[i].id;
+
+		$(idAba).removeClass("active");
+	}
+	
 
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(name).style.display = "block";
-    
+	var id = "#"+ evt.target.id;
+
+	$(id).addClass("active");
 }
