46d9ef949f710989529bf75eac9bc2653c9ae4bc
dudu631 <eduardo9ramos@hotmail.com>
2018-06-11 23:23:35 -0300

    Gramatica de declaracao de variavel

--- src/Warlock - Web Edition/scripts/grammar.js
+++ src/Warlock - Web Edition/scripts/grammar.js
@@ -10,30 +10,43 @@
         return {left:val, operator:first[0], right:rightAssoc(first[1], rest)};
     }   
 }
-
 Start = Block?
-
 Block= _"{"_ v:Sequence _"}"_ {return {left:v,operator:"block",right:null}}
 
-Sequence =
-	 l:Command op:';' r:Sequence { return {left:l,operator:"seq",right:r}; }
+Sequence =	  
+	 l:Command op:';' r:Sequence { return {left:l,operator:"seq",right:r}; }     
      / l:Command ';'? {return l}
+     / l:DeclSeq ';' r:Sequence { return{left:{left:l,operator:"seq", right:r},operator:"block",right:null};}
+     / l:DeclSeq ';'  { return{left:l,operator:"block",right:null};}
      
 Command =
 	Assignment
     /If
 	/While
-    
+
+DeclSeq=
+	Declaration';'DeclSeq
+    /Declaration
+
+Declaration =
+	l:declop __ r:IniSeq {return {left:l,operator:'decl',right:r}}
+
+IniSeq =
+	l:Ini',' r:IniSeq {return {left:l,operator:'iniSeq',right:r}}
+    /Ini 
+
+Ini =
+	l:ident op:'=' r:primary {return {left:l,operator:'ini',right:r}}
+ 
 If =
 	_ op:"if" __  l:BoolExpression r:Command "else" a:Command {return{left:l,operator:op.trim(),right:r,adit:a}}
     /_ op:"if" __ l:BoolExpression r:Command "else" a:Block {return{left:l,operator:op.trim(),right:r,adit:a}}
     /_ op:"if" __ l:BoolExpression r:Block "else" a:Command {return{left:l,operator:op.trim(),right:r,adit:a}}
     /_ op:"if" __ l:BoolExpression r:Block "else" a:Block {return{left:l,operator:op.trim(),right:r,adit:a}}
-
+    
 While =
-	_ op:"while" __ l:BoolExpression r:Block {return {left:{left:l,operator:op.trim(),right:r.left},operator:"block",right:null}}
-
-
+	_ op:"while" __ l:BoolExpression r:Block {return {left:l,operator:op.trim(),right:r}}
+    
 Expression
 	=  BoolExpression
     / AritExpression
@@ -41,16 +54,14 @@ Expression
 BoolExpression
 	= "("? l:AritExpression op:boolop r:AritExpression ")"? {return {left:l,operator:op,right:r}}    
 	/ Negate
-
+    
 Negate 
 	= v:neg rest:BoolExpression {return {left:rest, operator:v, right:null}}
     
 AritExpression
 	= E1
-
 E1 = v:(E2 (add / sub))* rest:E2
      { return leftAssoc(v, rest); }
-
 E2 = v:(primary (mul / div))* rest:primary
      { return leftAssoc(v, rest); }
  
@@ -61,16 +72,16 @@ primary
   = number
   / ident
   /BracketedExpression
-
+  
 BracketedExpression
   =	_ "("expression:Expression")" _  { return expression }
-
+  
 number
   = _ digits:[0-9]+ _  { return digits.join(""); }
-
+  
 ident 
     = _ head: [a-zA-Z] tail:[a-zA-Z0-9]*  _ {return head.concat(tail.join(""));}
-
+    
 boolop
 	= "==" { return "eq" }
     / ">=" { return "ge" }
@@ -82,22 +93,20 @@ boolop
     
 neg
 	= _"~"_ {return "neg"}
-
 add
     = "+" { return "add" }
-
 sub
     = "-" { return "sub" }
-
 mul 
     = "*" {return "mul"}
-
 div 
     = "/" {return "div"}
-
+declop
+    =_"const" {return "const"}
+    /_"var" {return "var"}
+    
 // optional whitespace
 _  = [ \\t\\r\\n]*
-
 // mandatory whitespace
 __ = [ \\t\\r\\n]+
 `;
